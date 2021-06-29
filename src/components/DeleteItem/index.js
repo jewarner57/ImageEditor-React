987057c43524c7a2b9d5ccaac13a2ea220b77fc4
Jonathan Warner <jewarner57@gmail.com>
2021-06-29 11:56:02 -0400

    feat: change icon for delete object button

--- src/components/DeleteItem/index.js
+++ src/components/DeleteItem/index.js
@@ -14,7 +14,7 @@ function DeleteItem(props) {
             dispatch(removeFromCanvas(selectedItem))
             dispatch(setSelectedObject(false))
           }}
-        icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" /></svg>}
+        icon={<svg width="30" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M5.662 23l-5.369-5.365c-.195-.195-.293-.45-.293-.707 0-.256.098-.512.293-.707l14.929-14.928c.195-.194.451-.293.707-.293.255 0 .512.099.707.293l7.071 7.073c.196.195.293.451.293.708 0 .256-.097.511-.293.707l-11.216 11.219h5.514v2h-12.343zm3.657-2l-5.486-5.486-1.419 1.414 4.076 4.072h2.829zm.456-11.429l-4.528 4.528 5.658 5.659 4.527-4.53-5.657-5.657z" /></svg>}
         tooltip={`Delete ${selectedItem.type}`}
       />
     </div>
